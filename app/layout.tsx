de3479e3cbe5b23629a46f24a42ced7ef6df6ec7
tomaszmilewski1 <tomasz_milewski1@outlook.com>
2026-09-04 14:45:21 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,11 @@ export const metadata = {
   description: "Aplikacja do skanowania i oceniania",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="pl">
       <body>{children}</body>
